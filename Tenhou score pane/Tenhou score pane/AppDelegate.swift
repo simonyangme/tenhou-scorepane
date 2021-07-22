f3b953dd35e0959d52fa7778681329451a164d7d
Simon Yang <git@simonyang.me>
2021-07-22 10:00:43 -0700

    Adding safari extension

---
+++ Tenhou score pane/Tenhou score pane/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Tenhou score pane
+//
+//  Created by Simon Yang on 7/12/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
